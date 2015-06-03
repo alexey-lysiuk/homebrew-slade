267e27b3b8e191fe57a4915d4367755e1a2de10d
alexey.lysiuk <alexey.lysiuk@gmail.com>
2015-06-03 18:30:28 +0300

    + min OS X version in SLADE formula

--- slade.rb
+++ slade.rb
@@ -5,12 +5,16 @@ class Slade < Formula
   homepage "http://slade.mancubus.net/"
   head "https://github.com/sirjuddington/SLADE.git"
 
+  # SFML newer than 2.1 requires OS X 10.7
+  # Although, it's not excplicitly declared in its formula at the moment
+  depends_on :macos => :lion  
+
   depends_on "cmake"
   depends_on "fluid-synth"
   depends_on "freeimage"
   depends_on "ftgl"
   depends_on "sfml"
-  
+
   #
   # NOTE: Cannot use default wxWidgets formula at the moment
   #       because of disabled mediactrl library for 64-bit target
@@ -24,7 +28,7 @@ class Slade < Formula
     system "make"
     prefix.install "SLADE.app"
   end
-  
+
   def caveats
     "SLADE.app installed to #{prefix}"
   end
